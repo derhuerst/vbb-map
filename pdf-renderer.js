5315696049ee635432d8d9f3555dc6d36aa2c129
Jannis R <mail@jannisr.de>
2016-12-20 00:49:34 +0100

    update deps, clean up code

--- pdf-renderer.js
+++ pdf-renderer.js
@@ -10,57 +10,55 @@ const _ = require('./helpers')
 const renderer = () => {
 	const pdf = new PDF()
 	pdf.addPage({margin: 50, size: [_.translate.w, _.translate.h]})
-	pdf.lineCap('round'); pdf.lineJoin('round')
+	pdf.lineCap('round')
+	pdf.lineJoin('round')
 
 	const renderedSegments = {}
 	const stationsToRender = {}
 
-	const draw = {
-		  line: (width, color, x1, y1, x2, y2) => {
-			pdf.lineWidth(width); pdf.strokeColor(color)
-			pdf.moveTo(x1, y1); pdf.lineTo(x2, y2); pdf.stroke()
-		}
-		, circle: (x, y, r, c) => {
-			pdf.lineWidth(r/5); pdf.fillColor(c)
-			pdf.circle(x, y, r); pdf.fill()
-		}
-		, text: (x, y, t, s, c) => {
-			pdf.fontSize(s); pdf.fillColor(c)
-			pdf.text(t, x, y)
-		}
+	const drawLine = (width, color, x1, y1, x2, y2) => {
+		pdf.lineWidth(width); pdf.strokeColor(color)
+		pdf.moveTo(x1, y1); pdf.lineTo(x2, y2); pdf.stroke()
+	}
+	const drawCircle = (x, y, r, c) => {
+		pdf.lineWidth(r/5); pdf.fillColor(c)
+		pdf.circle(x, y, r); pdf.fill()
+	}
+	const drawText = (x, y, t, s, c) => {
+		pdf.fontSize(s); pdf.fillColor(c)
+		pdf.text(t, x, y)
 	}
 
-	return {
+	const segment = (line, from, to) => {
+		if (renderedSegments[from.id + '-' + to.id]) return
+		renderedSegments[from.id + '-' + to.id] = true
 
-		segment: (line, from, to) => {
-			if (renderedSegments[from.id + '-' + to.id]) return
-			renderedSegments[from.id + '-' + to.id] = true
+		const {x: x1, y: y1} = _.translate(from.latitude, from.longitude)
+		const {x: x2, y: y2} = _.translate(to.latitude, to.longitude)
 
-			const {x: x1, y: y1} = _.translate(from.latitude, from.longitude)
-			const {x: x2, y: y2} = _.translate(to.latitude, to.longitude)
+		if (Math.max(x1, x2) <= _.translate.w && Math.min(x1, x2) >= 0
+		 && Math.max(y1, y2) <= _.translate.h && Math.min(y1, y2) >= 0)
+			drawLine(.5, _.color(line.name), x1, y1, x2, y2)
 
-			if (Math.max(x1, x2) <= _.translate.w && Math.min(x1, x2) >= 0
-			 && Math.max(y1, y2) <= _.translate.h && Math.min(y1, y2) >= 0)
-				draw.line(.5, _.color(line.name), x1, y1, x2, y2)
+		stationsToRender[from.id] = from
+		stationsToRender[to.id]   = to
+	}
 
-			stationsToRender[from.id] = from
-			stationsToRender[to.id]   = to
-		},
-
-		data: () => {
-			for (let id in stationsToRender) {
-				const station = stationsToRender[id]
-				const {x, y} = _.translate(station.latitude, station.longitude)
-				if (x <= _.translate.w && x >= 0
-				 && y <= _.translate.h && y >= 0) {
-					draw.circle(x, y, .5, '#000000')
-					draw.text(x + 2, y, shorten(station.name), 2, '#000000')
-				}
+	const data = () => {
+		for (let id in stationsToRender) {
+			const station = stationsToRender[id]
+			const {x, y} = _.translate(station.latitude, station.longitude)
+			if (x <= _.translate.w && x >= 0
+			 && y <= _.translate.h && y >= 0) {
+				drawCircle(x, y, .5, '#000000')
+				drawText(x + 2, y, shorten(station.name), 2, '#000000')
 			}
-			pdf.end()
-			return pdf
 		}
+		pdf.end()
+		return pdf
 	}
+
+	return {segment, data}
 }
 
 module.exports = renderer
--- render.js
+++ render.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const stations = require('vbb-stations')
+const stations = require('vbb-stations/full.json')
 const fs       = require('fs')
 const lines    = require('vbb-lines')
 const filter   = require('stream-filter')
@@ -8,29 +8,34 @@ const through  = require('through2')
 const shorten  = require('vbb-short-station-name')
 
 const _        = require('./helpers')
-const renderer = require('./pdf-renderer')
+const renderer = require('./pdf-renderer')()
 
 
 
 const all = {}
-stations('all').on('error', console.error)
-.on('data', (station) => {all[station.id] = station})
-.on('end', () => {
+for (let stationId in stations) {
+	const station = stations[stationId]
 
-	const render = renderer()
-	lines('all')
-	.pipe(filter((l) => l.type === 'subway'))
-	.on('data', (line) => {
-		for (let variant of line.variants) {
-			for (let i = 1; i < variant.length; i++) {
-				const from = all[variant[i - 1]]
-				const to   = all[variant[i]]
-				render.segment(line, from, to)
-			}
+	all[station.id] = station
+	if (station.stops) {
+		for (let stop of station.stops) all[stop.id] = stop
+	}
+}
+
+
+
+lines('all')
+.pipe(filter.obj((l) => l.type === 'subway'))
+.on('data', (line) => {
+	for (let variant of line.variants) {
+		for (let i = 1; i < variant.length; i++) {
+			const from = all[variant[i - 1]]
+			const to   = all[variant[i]]
+			renderer.segment(line, from, to)
 		}
-	})
-	.on('end', () => {
-		render.data().pipe(fs.createWriteStream('rendered/all.pdf'))
-		.on('finish', () => console.log('done'))
-	})
+	}
+})
+.on('end', () => {
+	renderer.data().pipe(fs.createWriteStream('rendered/all.pdf'))
+	.on('finish', () => console.log('done'))
 })
